e91c1ca3a9f4599ad05f6cb03b332234ea708b33
AdamWarfa <Adamwarfadk@gmail.com>
2024-03-05 17:12:48 +0100

    next step -> recipedto

---
+++ src/main/java/dat3/recipe/api/RecipeController.java
@@ -0,0 +1,27 @@
+package dat3.recipe.api;
+
+import dat3.recipe.service.RecipeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipes")
+public class RecipeController {
+
+    private RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    public List<RecipeDto> getAllRecipes(@RequestParam(required = false) String category) {
+        if(category != null) {
+            System.out.println("Category: " + category);
+        }
+        return recipeService.getAllRecipes(category);
+    }
+
+}
